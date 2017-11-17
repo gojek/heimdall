8823316916de748a52db0c5c97ee5b46dc4879e7
Rajeev N B <rajeev.bharshetty@go-jek.com>
2017-11-17 10:54:43 +0700

    Add basic examples of usage of clients

---
+++ examples/client.go
@@ -0,0 +1,53 @@
+package examples
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gojek-engineering/heimdall"
+	"github.com/pkg/errors"
+)
+
+const (
+	baseURL = "http://localhost:9090"
+)
+
+func httpClientUsage() error {
+	timeoutInMillis := 100
+	httpClient := heimdall.NewHTTPClient(timeoutInMillis)
+	headers := http.Header{}
+	headers.Set("Content-Type", "application/json")
+
+	httpClient.SetRetryCount(2)
+	httpClient.SetRetrier(heimdall.NewRetrier(heimdall.NewConstantBackoff(10)))
+
+	response, err := httpClient.Get(baseURL, headers)
+	if err != nil {
+		return errors.Wrap(err, "failed to make a request to server")
+	}
+
+	fmt.Printf("Response: %s", string(response.Body()))
+	return nil
+}
+
+func hystrixClientUsage() error {
+	httpClient := &http.Client{}
+
+	hystrixConfig := heimdall.NewHystrixConfig("MyCommand", heimdall.HystrixCommandConfig{
+		Timeout:                1100,
+		MaxConcurrentRequests:  100,
+		ErrorPercentThreshold:  25,
+		SleepWindow:            10,
+		RequestVolumeThreshold: 10,
+	})
+
+	hystrixClient := heimdall.NewHystrixHTTPClient(httpClient, hystrixConfig)
+	headers := http.Header{}
+	response, err := hystrixClient.Get(baseURL, headers)
+	if err != nil {
+		return errors.Wrap(err, "failed to make a request to server")
+	}
+
+	fmt.Printf("Response: %s", string(response.Body()))
+	return nil
+}
