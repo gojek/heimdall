1f36e488d155ce26a30f937188cadef7268fccf3
shubham.s <shubham.s@go-jek.com>
2018-03-24 15:47:48 +0530

    add specification for http client with context PATCH success with todo context

--- context_client_test.go
+++ context_client_test.go
@@ -163,3 +163,38 @@ func TestHTTPClientWithContextPutSuccessWithTODOContext(t *testing.T) {
 	assert.Equal(t, http.StatusOK, response.StatusCode)
 	assert.Equal(t, "{ \"response\": \"ok\" }", respBody(t, response))
 }
+
+func TestHTTPClientWithContextPatchSuccessWithTODOContext(t *testing.T) {
+	client := NewHTTPClientWithContext(10 * time.Millisecond)
+
+	requestBodyString := `{ "name": "heimdall" }`
+
+	dummyHandler := func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodPatch, r.Method)
+		assert.Equal(t, r.Header.Get("Content-Type"), "application/json")
+		assert.Equal(t, r.Header.Get("Accept-Language"), "en")
+
+		rBody, err := ioutil.ReadAll(r.Body)
+		require.NoError(t, err, "should not have failed to extract request body")
+
+		assert.Equal(t, requestBodyString, string(rBody))
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{ "response": "ok" }`))
+	}
+
+	server := httptest.NewServer(http.HandlerFunc(dummyHandler))
+	defer server.Close()
+
+	requestBody := bytes.NewReader([]byte(requestBodyString))
+
+	headers := http.Header{}
+	headers.Set("Content-Type", "application/json")
+	headers.Set("Accept-Language", "en")
+
+	response, err := client.Patch(context.TODO(), server.URL, requestBody, headers)
+	require.NoError(t, err, "should not have failed to make a PATCH request")
+
+	assert.Equal(t, http.StatusOK, response.StatusCode)
+	assert.Equal(t, "{ \"response\": \"ok\" }", respBody(t, response))
+}
