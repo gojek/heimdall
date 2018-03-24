d252ca19a1ed9f94ebd4b2458e23088883caf74b
shubham.s <shubham.s@go-jek.com>
2018-03-24 12:10:28 +0530

    add ClientWithContext interface to support http client with golang context support

--- client.go
+++ client.go
@@ -1,6 +1,7 @@
 package heimdall
 
 import (
+	"context"
 	"io"
 	"net/http"
 )
@@ -13,6 +14,19 @@ type Client interface {
 	Patch(url string, body io.Reader, headers http.Header) (*http.Response, error)
 	Delete(url string, headers http.Header) (*http.Response, error)
 	Do(req *http.Request) (*http.Response, error)
+
+	SetRetryCount(count int)
+	SetRetrier(retrier Retriable)
+}
+
+// ClientWithContext is a generic HTTP client interface which supports golang context
+type ClientWithContext interface {
+	Get(ctx context.Context, url string, headers http.Header) (*http.Response, error)
+	Post(ctx context.Context, url string, body io.Reader, headers http.Header) (*http.Response, error)
+	Put(ctx context.Context, url string, body io.Reader, headers http.Header) (*http.Response, error)
+	Patch(ctx context.Context, url string, body io.Reader, headers http.Header) (*http.Response, error)
+	Delete(ctx context.Context, url string, headers http.Header) (*http.Response, error)
+	Do(ctx context.Context, req *http.Request) (*http.Response, error)
 
 	SetRetryCount(count int)
 	SetRetrier(retrier Retriable)
