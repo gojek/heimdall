ef841d815f98236baee560b1558bc8e625f73c37
Soham Kamani <sohamkamani@gmail.com>
2018-07-31 17:09:04 +0530

    Add examples for hystrix client

--- hystrix/options_test.go
+++ hystrix/options_test.go
@@ -1,6 +1,8 @@
 package hystrix
 
 import (
+	"fmt"
+	"net/http"
 	"testing"
 	"time"
 
@@ -37,4 +39,90 @@ func TestOptionsHaveDefaults(t *testing.T) {
 	assert.Equal(t, 25, c.errorPercentThreshold)
 	assert.Equal(t, 10, c.sleepWindow)
 	assert.Equal(t, 10, c.requestVolumeThreshold)
+}
+
+func ExampleWithHTTPTimeout() {
+	c := NewClient(WithHTTPTimeout(5 * time.Second))
+	req, err := http.NewRequest(http.MethodGet, "https://www.gojek.io/", nil)
+	if err != nil {
+		panic(err)
+	}
+	res, err := c.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Response status : ", res.StatusCode)
+	// Output: Response status :  200
+}
+
+func ExampleWithHTTPTimeout_expired() {
+	c := NewClient(WithHTTPTimeout(1 * time.Millisecond))
+	req, err := http.NewRequest(http.MethodGet, "https://www.gojek.io/", nil)
+	if err != nil {
+		panic(err)
+	}
+	res, err := c.Do(req)
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+	fmt.Println("Response status : ", res.StatusCode)
+}
+
+func ExampleWithRetryCount() {
+	c := NewClient(WithHTTPTimeout(1*time.Millisecond), WithRetryCount(3))
+	req, err := http.NewRequest(http.MethodGet, "https://www.gojek.io/", nil)
+	if err != nil {
+		panic(err)
+	}
+	res, err := c.Do(req)
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+	fmt.Println("Response status : ", res.StatusCode)
+}
+
+type mockClient struct{}
+
+func (m *mockClient) Do(r *http.Request) (*http.Response, error) {
+	fmt.Println("mock client called")
+	return &http.Response{}, nil
+}
+
+func ExampleWithHTTPClient() {
+	m := &mockClient{}
+	c := NewClient(WithHTTPClient(m))
+	req, err := http.NewRequest(http.MethodGet, "https://www.gojek.io/", nil)
+	if err != nil {
+		panic(err)
+	}
+	_, _ = c.Do(req)
+	// Output: mock client called
+}
+
+type mockRetrier struct{}
+
+func (m *mockRetrier) NextInterval(attempt int) time.Duration {
+	fmt.Println("retry attempt", attempt)
+	return time.Millisecond
+}
+
+func ExampleWithRetrier() {
+	c := NewClient(WithHTTPTimeout(1*time.Millisecond), WithRetryCount(3), WithRetrier(&mockRetrier{}))
+	req, err := http.NewRequest(http.MethodGet, "https://www.gojek.io/", nil)
+	if err != nil {
+		panic(err)
+	}
+	res, err := c.Do(req)
+	if err != nil {
+		fmt.Println("error")
+		return
+	}
+	fmt.Println("Response status : ", res.StatusCode)
+	// Output: retry attempt 0
+	// retry attempt 1
+	// retry attempt 2
+	// retry attempt 3
+	// error
 }
