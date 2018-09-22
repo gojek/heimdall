5eb9b321c6b8d25c7416283a896be3ceae3dc95a
Rajat Varyani <rajat.varyani@outlook.com>
2018-09-22 15:44:26 +0530

    Add examples for http client retry mechanisms

--- examples/client.go
+++ examples/client.go
@@ -143,3 +143,107 @@ func customHystrixClientUsage() error {
 	fmt.Printf("Response: %s", string(respBody))
 	return nil
 }
+
+func httpClientWithExponentialBackoff() error {
+	initalTimeout := 2 * time.Millisecond
+	maxTimeout := 9 * time.Millisecond
+	exponentFactor := 2.0
+	maximumJitterInterval := 2 * time.Millisecond
+
+	backoff := heimdall.NewExponentialBackoff(initalTimeout, maxTimeout, exponentFactor, maximumJitterInterval)
+
+	retrier := heimdall.NewRetrier(backoff)
+
+	timeout := 1000 * time.Millisecond
+	client := httpclient.NewClient(
+		httpclient.WithHTTPTimeout(timeout),
+		httpclient.WithRetrier(retrier),
+		httpclient.WithRetryCount(3),
+	)
+
+	headers := http.Header{}
+	response, err := client.Get(baseURL, headers)
+	if err != nil {
+		return errors.Wrap(err, "failed to make a request to server")
+	}
+
+	defer response.Body.Close()
+
+	respBody, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return errors.Wrap(err, "failed to read response body")
+	}
+
+	fmt.Printf("Response: %s", string(respBody))
+	return nil
+}
+
+func httpClientWithConstantBackoff() error {
+	backOffInterval := 10 * time.Second
+	maximumJitterInterval := 2 * time.Millisecond
+
+	backoff := heimdall.NewConstantBackoff(backOffInterval, maximumJitterInterval)
+	retrier := heimdall.NewRetrier(backoff)
+
+	timeout := 1000 * time.Millisecond
+	client := httpclient.NewClient(
+		httpclient.WithHTTPTimeout(timeout),
+		httpclient.WithRetrier(retrier),
+		httpclient.WithRetryCount(3),
+	)
+
+	headers := http.Header{}
+	response, err := client.Get(baseURL, headers)
+	if err != nil {
+		return errors.Wrap(err, "failed to make a request to server")
+	}
+
+	defer response.Body.Close()
+
+	respBody, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return errors.Wrap(err, "failed to read response body")
+	}
+
+	fmt.Printf("Response: %s", string(respBody))
+	return nil
+}
+
+type linearBackoff struct {
+	backoffInterval int
+}
+
+func (lb *linearBackoff) Next(retry int) time.Duration {
+	if retry <= 0 {
+		return 0 * time.Millisecond
+	}
+	return time.Duration(retry*lb.backoffInterval) * time.Millisecond
+}
+
+func httpClientWithCustomBackoff() error {
+	backoff := &linearBackoff{100}
+	retrier := heimdall.NewRetrier(backoff)
+
+	timeout := 1000 * time.Millisecond
+	client := httpclient.NewClient(
+		httpclient.WithHTTPTimeout(timeout),
+		httpclient.WithRetrier(retrier),
+		httpclient.WithRetryCount(3),
+	)
+
+	headers := http.Header{}
+	response, err := client.Get(baseURL, headers)
+	if err != nil {
+		return errors.Wrap(err, "failed to make a request to server")
+	}
+
+	defer response.Body.Close()
+
+	respBody, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return errors.Wrap(err, "failed to read response body")
+	}
+
+	fmt.Printf("Response: %s", string(respBody))
+	return nil
+}
