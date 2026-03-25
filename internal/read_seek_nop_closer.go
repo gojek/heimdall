47cfba0aad4d247f015c7c77ebef315e3ebd349c
Anmol Chopra <anmol.chopra@gojek.com>
2026-03-25 11:57:54 +0530

    minor naming changes

--- internal/read_seek_nop_closer.go
+++ internal/read_seek_nop_closer.go
@@ -14,7 +14,7 @@ func BuildReadSeekCloser(body io.Reader) (io.ReadSeekCloser, error) {
 	// Do not check for io.ReadSeekCloser as we don't know if Close is a no-op or not.
 	// Instead, we check for io.ReadSeeker and wrap it with a no-op Close method.
 	if seeker, ok := body.(io.ReadSeeker); ok {
-		return newBytesNopCloser(seeker), nil
+		return newReadSeekNopCloser(seeker), nil
 	}
 
 	data, err := io.ReadAll(body)
@@ -23,30 +23,30 @@ func BuildReadSeekCloser(body io.Reader) (io.ReadSeekCloser, error) {
 	}
 
 	// return io.ReadSeekCloser so other wrappers can also reuse it if needed.
-	return newBytesNopCloser(bytes.NewReader(data)), nil
+	return newReadSeekNopCloser(bytes.NewReader(data)), nil
 }
 
-type readSeekerNopCloser struct {
+type readSeekNopCloser struct {
 	d io.ReadSeeker
 	m sync.Mutex
 }
 
-func (b *readSeekerNopCloser) Close() error { return nil }
+func (b *readSeekNopCloser) Close() error { return nil }
 
-func (b *readSeekerNopCloser) Read(p []byte) (n int, err error) {
+func (b *readSeekNopCloser) Read(p []byte) (n int, err error) {
 	b.m.Lock()
 	defer b.m.Unlock()
 
 	return b.d.Read(p)
 }
 
-func (b *readSeekerNopCloser) Seek(offset int64, whence int) (int64, error) {
+func (b *readSeekNopCloser) Seek(offset int64, whence int) (int64, error) {
 	b.m.Lock()
 	defer b.m.Unlock()
 
 	return b.d.Seek(offset, whence)
 }
 
-func newBytesNopCloser(data io.ReadSeeker) *readSeekerNopCloser {
-	return &readSeekerNopCloser{d: data}
+func newReadSeekNopCloser(data io.ReadSeeker) *readSeekNopCloser {
+	return &readSeekNopCloser{d: data}
 }
